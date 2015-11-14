05930b4cbc38686939c7f16959d12c74acbe0d88
Takeshi SASAKI <dagezi@gmail.com>
2015-11-14 15:54:28 +0900

    First commit.

---
+++ sample/src/androidTest/java/io/github/dagezi/gradle_android_9patch_plugin/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.github.dagezi.gradle_android_9patch_plugin;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
